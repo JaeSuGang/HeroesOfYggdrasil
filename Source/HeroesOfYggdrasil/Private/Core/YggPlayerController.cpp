e971220a17de39a9db7b08de424cfff46e59f865
Kai Kim <33kkm33@gmail.com>
2025-02-26 18:12:48 +0900

    김경민 : YggPlayerController 추가

---
+++ Source/HeroesOfYggdrasil/Private/Core/YggPlayerController.cpp
@@ -0,0 +1,5 @@
+// Coded By AssortRock Unreal Engine Class Project
+
+
+#include "Core/YggPlayerController.h"
+
---
+++ Source/HeroesOfYggdrasil/Public/Core/YggPlayerController.h
@@ -0,0 +1,17 @@
+// Coded By AssortRock Unreal Engine Class Project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "YggPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEROESOFYGGDRASIL_API AYggPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
