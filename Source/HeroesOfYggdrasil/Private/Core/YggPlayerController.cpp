4c525f4af496e605f332d43fbea28fbf14b5d37d
Kai Kim <33kkm33@gmail.com>
2025-02-27 11:21:39 +0900

    김경민 : PlayerController의 IGenericTeamAgentInterface상속

--- Source/HeroesOfYggdrasil/Private/Core/YggPlayerController.cpp
+++ Source/HeroesOfYggdrasil/Private/Core/YggPlayerController.cpp
@@ -3,3 +3,14 @@
 
 #include "Core/YggPlayerController.h"
 
+AYggPlayerController::AYggPlayerController(const FObjectInitializer& ObjectInitializer)
+    :
+    Super(ObjectInitializer)
+{
+	TeamID = FGenericTeamId{ 0 };
+}
+
+FGenericTeamId AYggPlayerController::GetGenericTeamId() const
+{
+    return TeamID;
+}
--- Source/HeroesOfYggdrasil/Public/Core/YggPlayerController.h
+++ Source/HeroesOfYggdrasil/Public/Core/YggPlayerController.h
@@ -4,14 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "GenericTeamAgentInterface.h"
 #include "YggPlayerController.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class HEROESOFYGGDRASIL_API AYggPlayerController : public APlayerController
+class HEROESOFYGGDRASIL_API AYggPlayerController : public APlayerController, public IGenericTeamAgentInterface
 {
 	GENERATED_BODY()
-	
+
+public:
+	AYggPlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+public:
+	FGenericTeamId GetGenericTeamId() const override;
+
+protected:
+	FGenericTeamId TeamID;
 };
