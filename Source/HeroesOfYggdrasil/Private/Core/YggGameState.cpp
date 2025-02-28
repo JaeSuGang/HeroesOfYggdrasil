628c62e6066f9ef928551faa4ad9bebba5af3b77
Kai Kim <33kkm33@gmail.com>
2025-02-28 10:45:08 +0900

    김경민 : 업데이트

--- Source/HeroesOfYggdrasil/Private/Core/YggGameState.cpp
+++ Source/HeroesOfYggdrasil/Private/Core/YggGameState.cpp
@@ -11,49 +11,4 @@ AYggGameState::AYggGameState(const FObjectInitializer& ObjectInitializer)
 
 }
 
-void AYggGameState::BeginPlay()
-{
-	Super::BeginPlay();
 
-	if (HasAuthority())
-	{
-		this->InitPlayerManager();
-		this->InitStageManager(nullptr);
-	}
-}
-
-void AYggGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
-{
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-
-	DOREPLIFETIME(AYggGameState, PlayerManager);
-	DOREPLIFETIME(AYggGameState, StageManager);
-}
-
-void AYggGameState::InitPlayerManager()
-{
-	if (HasAuthority())
-	{
-		PlayerManager = GetWorld()->SpawnActor<APlayerManager>(PlayerManagerClass);
-	}
-}
-
-APlayerManager* AYggGameState::GetPlayerManager() const
-{
-	return PlayerManager;
-}
-
-void AYggGameState::InitStageManager(TSubclassOf<AStageManager> _Test)
-{
-	if (HasAuthority())
-	{
-		StageManagerClass;
-
-		StageManager = GetWorld()->SpawnActor<AStageManager>(StageManagerClass);
-	}
-}
-
-AStageManager* AYggGameState::GetStageManager() const
-{
-	return StageManager;
-}
--- Source/HeroesOfYggdrasil/Public/Core/YggGameState.h
+++ Source/HeroesOfYggdrasil/Public/Core/YggGameState.h
@@ -4,11 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameState.h"
-#include "MainGame/StageManager.h"
-#include "MainGame/PlayerManager.h"
 #include "YggGameState.generated.h"
-
-class UClass;
 
 /**
  * 담당 코더 : 김경민
@@ -22,34 +18,5 @@ public:
 	AYggGameState(const FObjectInitializer& ObjectInitializer);
 
 
-protected:
-	void BeginPlay() override;
 
-	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
-
-public:
-	UFUNCTION(BlueprintCallable)
-	AStageManager* GetStageManager() const;
-
-	UFUNCTION(BlueprintCallable)
-	APlayerManager* GetPlayerManager() const;
-
-	UFUNCTION(BlueprintCallable)
-	void InitPlayerManager();
-
-	UFUNCTION(BlueprintCallable)
-	void InitStageManager(TSubclassOf<AStageManager> _Test);
-
-public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
-	UClass* PlayerManagerClass;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
-	UClass* StageManagerClass;
-
-	UPROPERTY(Replicated, VisibleInstanceOnly, BlueprintReadWrite)
-	APlayerManager* PlayerManager;
-
-	UPROPERTY(Replicated, VisibleInstanceOnly, BlueprintReadWrite)
-	AStageManager* StageManager;
 };
