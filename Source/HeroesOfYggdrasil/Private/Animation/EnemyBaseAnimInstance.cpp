f6d7d3c26809383d71fe66c3228dbde5e9881f3b
시혁 장 <wkdtl345@naver.com>
2025-03-05 03:30:29 +0900

    장시혁 : 적 애니메이션 테스트

--- Source/HeroesOfYggdrasil/Private/Animation/EnemyBaseAnimInstance.cpp
+++ Source/HeroesOfYggdrasil/Private/Animation/EnemyBaseAnimInstance.cpp
@@ -2,4 +2,54 @@
 
 
 #include "Animation/EnemyBaseAnimInstance.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Attribute/AttributeComponent.h"
+#include "Enemy/EnemyCharacter.h"
+
+void UEnemyBaseAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	Enemy = Cast<AEnemyCharacter>(TryGetPawnOwner());
+	if (Enemy == nullptr)
+	{
+		return;
+	}
+	CharacterMovementComponent = Enemy->GetCharacterMovement();
+}
+
+void UEnemyBaseAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaTime);
+	if (CharacterMovementComponent == nullptr)
+	{
+		return;
+	}
+
+	GroundSpeed = UKismetMathLibrary::VSizeXY(CharacterMovementComponent->Velocity);
+	LocomotionDirection = CalculateDirection(CharacterMovementComponent->Velocity, Enemy->GetActorRotation());
+}
+
+bool UEnemyBaseAnimInstance::NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck)
+{
+	FString ActorName = InActor->GetName();
+	FString ActorNameLeft = ActorName.Left(8);
+
+	FName TagName = TagToCheck.GetTagName();
+	
+	if (ActorNameLeft == FString("BP_Enemy") && TagName == FName("Monster.State.Strafing"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool UEnemyBaseAnimInstance::OwnerHaveTag(FGameplayTag _TagToCheck) const
+{
+	if (APawn* OwningPawn = TryGetPawnOwner())
+	{
+		return NativeDoesActorHaveTag(OwningPawn, _TagToCheck);
+	}
+	return false;
+}
 
--- Source/HeroesOfYggdrasil/Public/Animation/EnemyBaseAnimInstance.h
+++ Source/HeroesOfYggdrasil/Public/Animation/EnemyBaseAnimInstance.h
@@ -4,14 +4,43 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "GameplayTagContainer.h"
+
 #include "EnemyBaseAnimInstance.generated.h"
 
 /**
- * 
+ * 담당 : 장시혁
  */
+
+
+class AEnemyCharacter;
+class UCharacterMovementComponent;
+
+
 UCLASS()
 class HEROESOFYGGDRASIL_API UEnemyBaseAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaTime) override;
+	static bool NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck);
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	AEnemyCharacter* Enemy;
+
+	UPROPERTY(BlueprintReadOnly)
+	UCharacterMovementComponent* CharacterMovementComponent;
+
+	UPROPERTY(BlueprintReadOnly)
+	float GroundSpeed;
+
+	UPROPERTY(BlueprintReadOnly)
+	float LocomotionDirection;
+
+
+	UFUNCTION(BlueprintPure, meta = (BlueprintThreadSafe))
+	bool OwnerHaveTag(FGameplayTag _TagToCheck) const;
 };
