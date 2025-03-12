352731127e9e3d7028800ce2441a1c269977c537
kjhgme <kjhgme@naver.com>
2025-03-12 17:35:29 +0900

    김지호 : Hero Data 수정.

--- Source/HeroesOfYggdrasil/Public/Data/YggStructData.h
+++ Source/HeroesOfYggdrasil/Public/Data/YggStructData.h
@@ -34,7 +34,11 @@ struct FHeroBaseStatusInfoRow : public FTableRowBase
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
-	float Hp;
+	float CurHp = 100.0f; 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
+	float MaxHp = 100.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
+	float HpRegen = 1.0f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
 	float GroundSpeedRate =1.0f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
@@ -42,7 +46,17 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
 	float JumpRate = 1.0f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
-	int MaxAttackIndex;
+	int CurAttackIndex = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
+	int MaxAttackIndex = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
+	float AttPower = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
+	float DefPower = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
+	float CriticalChance = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
+	float CriticalDamage = 1.0f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
 	FHeroSkillInfoRow SkillQInfo;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "YggData")
