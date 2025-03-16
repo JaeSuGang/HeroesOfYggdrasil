93c60bd379570cf89e016a229c82563663115cbc
LeeChangJun <naromine@naver.com>
2025-03-16 17:29:31 +0900

    이창준 : 오류 수정

--- Source/HeroesOfYggdrasil/Public/MainGame/UI/YggAbilityPlusUserWidget.h
+++ Source/HeroesOfYggdrasil/Public/MainGame/UI/YggAbilityPlusUserWidget.h
@@ -16,18 +16,18 @@ class HEROESOFYGGDRASIL_API UYggAbilityPlusUserWidget : public UYggUserWidget
 {
 	GENERATED_BODY()
 	
-public:
-	UPROPERTY(EditAnywhere, Category = YGG)
-	void PlusButtonEvent();
-
-protected:
-	void NativeOnInitialized() override;
-
-
-private:
-	UPROPERTY(meta = (BindWidget))
-	UButton* PlusButton;
-
-	UPROPERTY(EditAnywhere, Category = YGG)
-	TSubclassOf<UUserWidget> AbilityWidgetClass;
+//public:
+//	UPROPERTY(EditAnywhere, Category = YGG)
+//	void PlusButtonEvent();
+//
+//protected:
+//	void NativeOnInitialized() override;
+//
+//
+//private:
+//	UPROPERTY(meta = (BindWidget))
+//	UButton* PlusButton;
+//
+//	UPROPERTY(EditAnywhere, Category = YGG)
+//	TSubclassOf<UUserWidget> AbilityWidgetClass;
 };
