673e1da1dded17910c03a5970b252410f273758b
Kai Kim <33kkm33@gmail.com>
2025-03-17 15:42:54 +0900

    김경민 : Attribute Component 보충

--- Source/HeroesOfYggdrasil/Private/Attribute/CharacterAttributeComponent.cpp
+++ Source/HeroesOfYggdrasil/Private/Attribute/CharacterAttributeComponent.cpp
@@ -15,6 +15,13 @@ void UCharacterAttributeComponent::Server_TakeDamage_Implementation(float fAmoun
 	}
 }
 
+void UCharacterAttributeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ClientDelegate_OnHealthChanged.AddDynamic(this, &UCharacterAttributeComponent::Foo);
+}
+
 void UCharacterAttributeComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
@@ -23,11 +30,42 @@ void UCharacterAttributeComponent::GetLifetimeReplicatedProps(TArray<FLifetimePr
 	DOREPLIFETIME(UCharacterAttributeComponent, MaxHp);
 }
 
+void UCharacterAttributeComponent::Server_SetMaxHp_Implementation(float fAmount)
+{
+	MaxHp = fAmount;
+}
+
+void UCharacterAttributeComponent::Server_SetHp_Implementation(float fAmount)
+{
+	Hp = fAmount;
+	if (GetWorld()->GetAuthGameMode())
+	{
+		OnRep_Hp();
+	}
+}
+
+float UCharacterAttributeComponent::GetHp() const
+{
+	return Hp;
+}
+
+float UCharacterAttributeComponent::GetMaxHp() const
+{
+	return MaxHp;
+}
+
+void UCharacterAttributeComponent::Foo()
+{
+
+}
+
 void UCharacterAttributeComponent::OnRep_Hp()
 {
+	if (GetOwner()->HasAuthority())
+		ServerDelegate_OnHealthChanged.Broadcast();
+
 	if (GetOwner()->HasLocalNetOwner())
-	{
 		ClientDelegate_OnHealthChanged.Broadcast();
-	}
+
 	MulticastDelegate_OnHealthChanged.Broadcast();
 }
--- Source/HeroesOfYggdrasil/Public/Attribute/CharacterAttributeComponent.h
+++ Source/HeroesOfYggdrasil/Public/Attribute/CharacterAttributeComponent.h
@@ -19,20 +19,46 @@ class HEROESOFYGGDRASIL_API UCharacterAttributeComponent : public UAttributeComp
 public:
 
 protected:
+	void BeginPlay() override;
 	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 public:
 	UFUNCTION(Server, Reliable, BlueprintCallable)
 	void Server_TakeDamage(float fAmount);
 
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void Server_SetHp(float fAmount);
+
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void Server_SetMaxHp(float fAmount);
+
 public:
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetHp() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetMaxHp() const;
+
+
+
+public:
+	/* 체력이 변했을 때 서버 컴퓨터만 실행되는 델리게이트 */
+	UPROPERTY(BlueprintAssignable)
+	FOnHealthChanged ServerDelegate_OnHealthChanged;
+
+	/* 체력이 변했을 때 캐릭터의 소유자만 실행되는 델리게이트 */
 	UPROPERTY(BlueprintAssignable)
 	FOnHealthChanged ClientDelegate_OnHealthChanged;
 
+	/* 체력이 변했을 때 모두가 실행되는 델리게이트*/
 	UPROPERTY(BlueprintAssignable)
 	FOnHealthChanged MulticastDelegate_OnHealthChanged;
 
 public:
+	UFUNCTION()
+	void Foo();
+
+
 	UFUNCTION()
 	void OnRep_Hp();
 
