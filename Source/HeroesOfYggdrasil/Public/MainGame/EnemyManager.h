54cd3a715db13519cd5b74f11f6c0269b8061cef
Kai Kim <33kkm33@gmail.com>
2025-03-17 18:27:53 +0900

    김경민 : EnemyManager를 통한 적 애니메이션 상태 초기화 자동화

--- Source/HeroesOfYggdrasil/Public/MainGame/EnemyManager.h
+++ Source/HeroesOfYggdrasil/Public/MainGame/EnemyManager.h
@@ -18,14 +18,12 @@ class HEROESOFYGGDRASIL_API AEnemyManager : public AInfo
 
 	AActor* CreateMonster(const FString& _ItemName, FVector _OriginPos);
 
+	UFUNCTION(Server, Reliable)
 	void NetSyncMonster();
 
 protected:
-	
 	virtual void BeginPlay() override;
 
 public:
-
 	TArray<class AEnemyCharacter*> AllEnemyCharacter;
-
 };
