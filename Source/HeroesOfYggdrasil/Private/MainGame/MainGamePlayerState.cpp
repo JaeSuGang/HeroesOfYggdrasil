75a1daca7a99a73dcf2d699a8a8f5d8866605c02
Kai Kim <33kkm33@gmail.com>
2025-03-31 18:29:53 +0900

    김경민 : 업그레이드 기능 추가 구현

--- Source/HeroesOfYggdrasil/Private/MainGame/MainGamePlayerState.cpp
+++ Source/HeroesOfYggdrasil/Private/MainGame/MainGamePlayerState.cpp
@@ -5,7 +5,39 @@
 
 #include "Net/UnrealNetwork.h"
 
+#include "Upgrade/Upgrades.h"
+
+void AMainGamePlayerState::ServerSetPlayerName_Implementation(const FString& name)
+{
+	AMainGamePlayerState::SetPlayerName(name);
+
+}
+
 void AMainGamePlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AMainGamePlayerState, UpgradePoints);
+}
+
+int AMainGamePlayerState::GetUpgradePoints() const
+{
+	return UpgradePoints;
+}
+
+UHeroUpgradeBase* AMainGamePlayerState::InstantiateHeroUpgrade(TSubclassOf<UHeroUpgradeBase> NewHeroUpgradeClass)
+{
+	return NewObject<UHeroUpgradeBase>(NewHeroUpgradeClass);
+}
+
+void AMainGamePlayerState::SetUpgradePoints_Implementation(int NewPoints)
+{
+	UpgradePoints = NewPoints;
+	ClientDelegate_OnUpgradePointsChanged.Broadcast(UpgradePoints);
+}
+
+void AMainGamePlayerState::AddUpgradePoints_Implementation(int PointsToAdd)
+{
+	UpgradePoints += PointsToAdd;
+	ClientDelegate_OnUpgradePointsChanged.Broadcast(UpgradePoints);
 }
--- Source/HeroesOfYggdrasil/Public/Core/YggPlayerState.h
+++ Source/HeroesOfYggdrasil/Public/Core/YggPlayerState.h
@@ -6,7 +6,7 @@
 #include "GameFramework/PlayerState.h"
 #include "YggPlayerState.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnUpgradePointsChangedDelegate, int, newType);
+
 
 /**
  * 담당 코더 : 김경민
@@ -19,27 +19,4 @@ class HEROESOFYGGDRASIL_API AYggPlayerState : public APlayerState
 public:
 	AYggPlayerState();
 
-protected:
-	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
-
-public:
-	UFUNCTION(Server, Reliable, BlueprintCallable)
-	void AddUpgradePoints(int PointsToAdd);
-
-	UFUNCTION(Server, Reliable, BlueprintCallable)
-	void SetUpgradePoints(int NewPoints);
-
-	UFUNCTION(BlueprintCallable)
-	int GetUpgradePoints() const;
-
-	UFUNCTION(Server, Reliable, BlueprintCallable)
-	void ServerSetPlayerName(const FString& name);
-
-public:
-	UPROPERTY(BlueprintAssignable, BlueprintReadWrite)
-	FOnUpgradePointsChangedDelegate ClientDelegate_OnUpgradePointsChanged;
-
-protected:
-	UPROPERTY(Replicated)
-	int UpgradePoints;
 };
