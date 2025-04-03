e688ea8eb520615bed1a9ddd760d7a6795cdc3b7
Marron0510 <wkdtl345@naver.com>
2025-04-03 16:15:45 +0900

    김성훈 : 업데이트

--- Source/HeroesOfYggdrasil/Public/Component/SceneComponent/YggCapsuleComponent.h
+++ Source/HeroesOfYggdrasil/Public/Component/SceneComponent/YggCapsuleComponent.h
@@ -9,6 +9,21 @@
 /**
  * 
  */
+UENUM(BlueprintType)
+enum class ECharacterType :uint8
+{
+	Player UMETA(DisplayName = "Player"),
+	Monster UMETA(DisplayName = "Monster"),
+};
+
+UENUM(BlueprintType)
+enum class EDamageType :uint8
+{
+	Normal UMETA(DisplayName = "Normal"),
+	Tick UMETA(DisplayName = "Tick"),
+};
+
+
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class HEROESOFYGGDRASIL_API UYggCapsuleComponent : public UCapsuleComponent
 {
@@ -18,4 +33,9 @@ public:
 	virtual void CollisionOn();
 	UFUNCTION(BlueprintCallable)
 	virtual void CollisionOff();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UYggCapsuleComponent")
+	ECharacterType CharacterType;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UYggCapsuleComponent")
+	EDamageType DamageType;
+
 };
