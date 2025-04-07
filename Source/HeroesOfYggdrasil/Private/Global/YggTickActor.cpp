2fb5479c4b0feaa5a0e412af6fc608c653ed418b
Marron0510 <wkdtl345@naver.com>
2025-04-07 13:54:14 +0900

    장시혁 : 오류수정

--- Source/HeroesOfYggdrasil/Private/Global/YggTickActor.cpp
+++ Source/HeroesOfYggdrasil/Private/Global/YggTickActor.cpp
@@ -19,14 +19,15 @@ AYggTickActor::AYggTickActor()
         //StatusTickCollision->OnComponentBeginOverlap.AddDynamic(this, &AYggTickActor::OverLapBegin);
     }
 
-    FStatusTickDataRow* Data = StatusTickDataTable->FindRow<FStatusTickDataRow>(FName("Poison"), nullptr);
+
 }
 
 // Called when the game starts or when spawned
 void AYggTickActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+    FStatusTickDataRow* Data = StatusTickDataTable->FindRow<FStatusTickDataRow>(FName("Poison"), nullptr);
 }
 
 // Called every frame
@@ -38,5 +39,5 @@ void AYggTickActor::Tick(float DeltaTime)
 
 void AYggTickActor::OverLapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-
+    int a = 0;
 }
