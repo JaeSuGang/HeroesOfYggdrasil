4327480aa1693f74199f13770e133c6fcb596f13
bitss . <donrang@naver.com>
2025-04-15 12:08:25 +0900

    신동민 : 나무

--- Source/HeroesOfYggdrasil/Private/NPC/Yggdrasil.cpp
+++ Source/HeroesOfYggdrasil/Private/NPC/Yggdrasil.cpp
@@ -7,8 +7,8 @@
 
 AYggdrasil::AYggdrasil()
 {
-	RootComponent = CreateDefaultSubobject<USceneComponent>("Root");
 	CharacterAttributeComponent = CreateDefaultSubobject<UCharacterAttributeComponent>("Attribute");
 	CharacterAttributeComponent->MaxHP = 100.f;
 	CharacterAttributeComponent->HP = 100.f;
+
 }
