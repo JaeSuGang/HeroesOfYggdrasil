c3e038899199d14e1470c401e7951a0b56fd0106
kjhgme <kjhgme@naver.com>
2025-04-24 04:07:42 +0900

    김지호 : Aurora SkillE, Fly temp.

--- Source/HeroesOfYggdrasil/Public/Player/YggHeroAurora.h
+++ Source/HeroesOfYggdrasil/Public/Player/YggHeroAurora.h
@@ -9,8 +9,6 @@
 /**
  * 
  */
-class UNiagaraSystem;
-class UNiagaraComponent;
 
 UCLASS()
 class HEROESOFYGGDRASIL_API AYggHeroAurora : public AYggHero
@@ -30,23 +28,27 @@ protected:
 	virtual void Attack(const FInputActionValue& Value) override;
 	virtual void EndAttack(const FInputActionValue& Value) override;
 
-	void Fly(const FInputActionValue& Value);
-
 	virtual void SkillQ(const FInputActionValue& Value) override;
 	virtual void SkillE(const FInputActionValue& Value) override;
 	virtual void SkillR(const FInputActionValue& Value) override;
 
 	virtual void Roll(const FInputActionValue& Value) override;
 
+	virtual void Jump() override;
+
+public:
+	void MagicCircleOn();
+	void MagicCircleOff();
+
 private:
 	UPROPERTY()
 	bool bIsSkillE = false;
 
 	UPROPERTY(EditAnywhere)
-	UMaterialInterface* SkillRDecalMaterial;
+	UMaterialInterface* SkillEDecalMaterial;
 
 	UPROPERTY()
-	UDecalComponent* SkillRDecal;
+	UDecalComponent* SkillEDecal;
 
 public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "YggHero")
@@ -55,4 +57,22 @@ public:
 	TObjectPtr<class UYggAttackCapsuleComponent> SkillEAttackCapsuleComponent;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "YggHero")
 	TObjectPtr<class UYggAttackCapsuleComponent> SkillRAttackCapsuleComponent;
+
+	/** 최대 제트팩 연료량(초 단위) */
+	float MaxJetpackFuel = 5.0f;
+	/** 현재 남은 연료량 */
+	float CurrentFuel = MaxJetpackFuel;
+	/** 제트팩 추진력 (Upward Acceleration) */
+	float JetpackThrust = 2000.0f;
+	/** 연료 소모 속도 (초당) */
+	float FuelConsumptionRate = 1.0f;
+	/** 연료 충전 속도 (초당) */
+	float FuelRechargeRate = 0.5f;
+	/** 제트팩 사용 중 플래그 */
+	bool bIsJetpacking = false;
+
+	UFUNCTION()
+	void JetpackOn(const FInputActionValue& Value);
+	UFUNCTION()
+	void JetpackOff(const FInputActionValue& Value);
 };
