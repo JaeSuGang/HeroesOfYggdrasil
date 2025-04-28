cebe093d8db0d8a5b97edf6a42aaae7379def976
이창준 <naromine@naver.com>
2025-04-28 18:08:00 +0900

    이창준 : Ability선택창 업데이트

---
+++ Source/HeroesOfYggdrasil/Public/MainGame/UI/YggVictoryUserWidget.h
@@ -0,0 +1,49 @@
+// Coded By AssortRock Unreal Engine Class Project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainGame/UI/YggUserWidget.h"
+#include "YggVictoryUserWidget.generated.h"
+
+class UTextBlock;
+class UWidgetAnimation;
+
+/**
+ * 
+ */
+UCLASS()
+class HEROESOFYGGDRASIL_API UYggVictoryUserWidget : public UYggUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	void Init();
+
+
+protected:
+	virtual void NativeOnInitialized() override;
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* PlayTime;
+	
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurWave;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* WaveProgress;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* MDeathCount;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* PDeathCount;
+
+	UPROPERTY(meta = (BindWidgetAnim), Transient)
+	UWidgetAnimation* FirstAnim;
+};
