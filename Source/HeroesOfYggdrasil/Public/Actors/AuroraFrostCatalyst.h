e0b351bb09a866591f758d3eb33c3647bbfd5029
kjhgme <kjhgme@naver.com>
2025-04-28 13:37:18 +0900

    김지호 : Aurora SkillE temp.

--- Source/HeroesOfYggdrasil/Public/Actors/AuroraFrostCatalyst.h
+++ Source/HeroesOfYggdrasil/Public/Actors/AuroraFrostCatalyst.h
@@ -28,15 +28,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION()
-	void OnCatalystOverlapBegin(
-		UPrimitiveComponent* OverlappedComp,
-		AActor* OtherActor,
-		UPrimitiveComponent* OtherComp,
-		int32 OtherBodyIndex,
-		bool bFromSweep,
-		const FHitResult& SweepResult
-	);
-
+	void OnCatalystHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UCapsuleComponent* CatalystCapsule;
