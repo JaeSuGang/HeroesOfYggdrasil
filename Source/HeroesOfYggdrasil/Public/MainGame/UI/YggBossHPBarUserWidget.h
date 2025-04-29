a561dde2c9f8e79b3d08ddd9a9f011f18f6ece03
이창준 <naromine@naver.com>
2025-04-29 14:29:39 +0900

    이창준 : 업데이트

---
+++ Source/HeroesOfYggdrasil/Public/MainGame/UI/YggBossHPBarUserWidget.h
@@ -0,0 +1,17 @@
+// Coded By AssortRock Unreal Engine Class Project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainGame/UI/YggUserWidget.h"
+#include "YggBossHPBarUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEROESOFYGGDRASIL_API UYggBossHPBarUserWidget : public UYggUserWidget
+{
+	GENERATED_BODY()
+	
+};
